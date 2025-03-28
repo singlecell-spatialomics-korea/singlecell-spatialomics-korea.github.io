3c29d55f7a9276479ff85e1ae38667a302b35a6d
Jeongbin Park <jeongbin.park@pusan.ac.kr>
2025-03-28 20:22:20 +0900

    update

--- src/routes/(subpages)/event/[...path]/+page.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export function load({ params }) {
-    const { path } = params;
-    const newPath = path.replace(/\//g, '-').replace(/\.html$/, '');
-    throw redirect(302, `/events/${newPath}`);
-}
